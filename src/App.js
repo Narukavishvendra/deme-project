6f68b97af782f30f10704459fb4a1fe03cfbfd0e
Vishvendra Singh Naruka <vishvendrasinghnaruka@Vishvendras-MacBook-Air.local>
2024-02-26 19:27:53 +0530

    new commit 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Router>
       <Routes>
-        <Route exact path="/" element={<Dasboard />}></Route>
+        <Route exact path="/demo-project" element={<Dasboard />}></Route>
       </Routes>
       </Router>
 
